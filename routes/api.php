b585edb08246bc981d7aac520a96b20c62ecd942
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-13 05:05:49 +0800

    install sanctum and created user registration and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 
 
-Route::apiResource('users', UserController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('users', UserController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
