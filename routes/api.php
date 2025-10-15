98a049bd43ac4afc90c02f7dea42ea48215c2bae
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-16 03:16:50 +0800

    Added Roles in API route

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
 
+use App\Http\Controllers\Api\RolesController;
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class);
+    Route::apiResource('roles', RolesController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
