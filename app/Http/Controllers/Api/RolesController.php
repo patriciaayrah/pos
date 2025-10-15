7b62ec40ecea2f89e94ee85529f5f8ff87eb7cb2
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-16 03:16:35 +0800

    Roles create and list

---
+++ app/Http/Controllers/Api/RolesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * List all roles
+     */
+    public function index()
+    {
+        return response()->json(Role::all(), 200);
+    }
+
+    /**
+     * Create Roles
+     * 
+     */
+    public function store(Request $request){
+
+        return $role = Role::create(['name' => $request->name]);
+
+        return response()->json($role, 201);
+        
+    }
+}
