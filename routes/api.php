5d0c4f6d197e85a9795915e0433410a19ed25f5c
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-16 17:36:09 +0800

    Added role middleware

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
+use Spatie\Permission\Models\Role;
+
 
 use App\Http\Controllers\Api\RolesController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('roles', RolesController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware('role:admin|owner|superadmin')->apiResource('users', UserController::class);
+    Route::middleware('role:superadmin')->apiResource('roles', RolesController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
