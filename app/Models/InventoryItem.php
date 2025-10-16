c752920d483e62a1d3c433d6b223f4d2133a044a
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-17 03:34:48 +0800

    Inventory Management

---
+++ app/Models/InventoryItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class InventoryItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'weight',
+        'unit',
+    ];
+
+    protected $guard_name = 'api';
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\InventoryItemController;
 use Spatie\Permission\Models\Role;
 
 
@@ -13,7 +14,12 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    
     Route::middleware('role:admin|owner|superadmin')->apiResource('users', UserController::class);
     Route::middleware('role:superadmin')->apiResource('roles', RolesController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    //INVENTORY MODULE
+    Route::middleware('role:admin|owner|superadmin')->apiResource('inventory-items', InventoryItemController::class);
+
 });
