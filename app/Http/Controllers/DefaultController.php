93f83f99bf9afdb28f57147a8ee2a0f08c1ce705
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-17 13:31:56 +0800

    Clean up code

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -84,14 +84,13 @@ class Usercontroller extends Controller
      */
     public function update(Request $request, $id)
     {
-         $check = $this->permission(['admin', 'owner']);
+         $check = $this->permission(['admin', 'owner', 'superadmin']);
         if($check !== true) { return $check; }
 
         $user = User::find($id);
         if(!$user) return response()->json(['message' => 'User not found'], 404);
 
         $user->update($request->only(['name', 'email']));
-        $user->assignRole($request->role);
         return response()->json($user, 200);
     }
 
