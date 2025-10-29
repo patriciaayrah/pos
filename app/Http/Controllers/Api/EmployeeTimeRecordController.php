562cfd62fe9c0892a67528d68ff1d96720e1686a
patriciaayrah <ayrah0421.pao@gmail.com>
2025-10-29 23:39:48 +0800

    Night shift remarks

--- app/Http/Controllers/Api/EmployeeTimeRecordController.php
+++ app/Http/Controllers/Api/EmployeeTimeRecordController.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\EmployeeTimeRecord;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Support\Facades\Auth;
 
 use Carbon\Carbon;
@@ -92,13 +89,17 @@ class EmployeeTimeRecordController extends Controller
         $time_record = EmployeeTimeRecord::find($id);
         if(!$time_record) return response()->json(['message' => 'Time record not found'], 404);
 
-        //Compute the total hours worked
+          //Compute the total hours worked
+        $total_hours = "";
         if($request->record_type == "time_out"){
-
+            $total_hours = $this->total_hours($time_record);
         }
+        
+
         $time_record->update([
             $request->record_type => now(),
-            'remarks' => $time_record->remarks.", ".$remarks
+            'remarks' => $time_record->remarks.", ".$remarks,
+            'hours_worked' => $total_hours
         ]);
 
         return response()->json($time_record, 200);
@@ -125,12 +126,40 @@ class EmployeeTimeRecordController extends Controller
         $employee_schedule = $user->employee_schedule;
         $remarks = "";
         $timestamp = now(); // current time of login
+        $time_in_schedule = "";
+        $time_out_schedule = "";
 
         if ($employee_schedule === 'morning') {
+            //TIME IN
+            $time_in_hr = "10";
+            $time_in_min = "0";
+            $time_in_sec = "0";
+            //TIME OUT
+            $time_out_hr = "20";
+            $time_out_min = "0";
+            $time_out_sec = "0";
+            //BREAK OUT
+            $break_out_hr = "18";
+            $break_out_min = "30";
+            $break_out_sec = "0";
+        }else if($employee_schedule === 'night'){
+            //TIME IN
+            $time_in_hr = "15";
+            $time_in_min = "0";
+            $time_in_sec = "0";
+            //TIME OUT
+            $time_out_hr = "0";
+            $time_out_min = "0";
+            $time_out_sec = "0";
+            //BREAK OUT
+            $break_out_hr = "19";
+            $break_out_min = "30";
+            $break_out_sec = "0";
+        }
 
             if ($data === 'time_in') {
                 // Morning shift start time (10:00 AM)
-                $shift_start = Carbon::today()->setTime(10, 0, 0);
+                $shift_start = Carbon::today()->setTime($time_in_hr, $time_in_min, $time_in_sec);
 
                 // Grace period: +10 mins
                 $grace_period = $shift_start->copy()->addMinutes(10);
@@ -141,7 +170,7 @@ class EmployeeTimeRecordController extends Controller
 
             } elseif ($data === 'time_out') {
                 // Morning shift end time (8:00 PM)
-                $shift_end = Carbon::today()->setTime(20, 0, 0);
+                $shift_end = Carbon::today()->setTime($time_out_hr, $time_out_min, $time_out_sec);
 
                 $remarks = $timestamp->lessThan($shift_end)
                     ? 'Early out'
@@ -149,16 +178,37 @@ class EmployeeTimeRecordController extends Controller
 
             } elseif ($data === 'break_out') {
                 // Break should end by 6:30 PM
-                $break_limit = Carbon::today()->setTime(18, 30, 0);
+                $break_limit = Carbon::today()->setTime($break_out_hr, $break_out_min, $break_out_sec);
 
                 $remarks = $timestamp->greaterThan($break_limit)
                     ? 'Overbreak'
                     : '';
             }
-        }
 
         return $remarks;
         
+    }
+
+    public function total_hours($data){
+
+        $time_in = Carbon::parse($data->time_in);
+        $time_out = Carbon::parse($data->time_out);
+        $break_in = Carbon::parse($data->break_in);
+        $break_out = Carbon::parse($data->break_out);
+        $total_hours = "";
+
+        // Compute durations
+        $total_minutes_worked = $time_in->diffInMinutes($time_out);
+        $break_minutes = $break_out->diffInMinutes($break_in);
+
+        $net_minutes = $total_minutes_worked - $break_minutes;
+
+        $total_hours = floor($net_minutes / 60);
+        $total_minutes = $net_minutes % 60;
+
+        $total_time_worked = sprintf('%02d:%02d', $total_hours, $total_minutes);
+
+        return $total_time_worked;
     }
 
 }
